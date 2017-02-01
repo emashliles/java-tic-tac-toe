45bdd568251588effd376f3e93b9277449b69e77
EA <emashliles@gmail.com>
2017-02-01 15:12:38 +0000

    Handle selection that is too large.

--- test/TurnUITests.java
+++ test/TurnUITests.java
@@ -2,6 +2,7 @@ import com.TTT.Board;
 import com.TTT.BoardPrinter;
 import com.TTT.Game;
 import com.TTT.TurnUI;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -11,13 +12,22 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class TurnUITests {
+
+    private Board board;
+    private Game game;
+    private BoardPrinter printer;
+
+    @Before
+    public void setUp() throws Exception {
+       board = new Board();
+       game = new Game(board);
+       printer = new BoardPrinter(System.out);
+    }
+
     @Test
     public void canAlternateTurns() {
-        BoardPrinter printer = new BoardPrinter(System.out);
         ByteArrayInputStream in = new ByteArrayInputStream("1\n2\n".getBytes());
         TurnUI turns = new TurnUI(printer, System.out, in);
-        Board board = new Board();
-        Game game = new Game(board);
 
         turns.takeTurn(board, game);
         turns.takeTurn(board, game);
@@ -27,14 +37,24 @@ public class TurnUITests {
 
     @Test
     public void asksForInputAgainIfInputIsText() {
-        BoardPrinter printer = new BoardPrinter(System.out);
         ByteArrayInputStream in = new ByteArrayInputStream(("invalidInput\n2").getBytes());
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream out = new PrintStream(outputStream);
 
         TurnUI turns = new TurnUI(printer, out, in);
-        Board board = new Board();
-        Game game = new Game(board);
+
+        turns.takeTurn(board, game);
+
+        assertEquals(outputStream.toString(), "Please enter a space number: Invalid input. Please enter a space number: ");
+    }
+
+    @Test
+    public void asksForInputAgainIfInputIsTooLarge() {
+        ByteArrayInputStream in = new ByteArrayInputStream(("9999999999999999999999999999\n7").getBytes());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+
+        TurnUI turns = new TurnUI(printer, out, in);
 
         turns.takeTurn(board, game);
 
