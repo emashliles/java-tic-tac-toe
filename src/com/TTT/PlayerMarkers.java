8781be7119183a6e551a576b9c0ff1ab158e301f
EA <emashliles@gmail.com>
2017-02-09 13:56:12 +0000

    Remove needless concatonation.

--- src/com/TTT/PlayerMarkers.java
+++ src/com/TTT/PlayerMarkers.java
@@ -1,8 +1,8 @@
 package com.TTT;
 
 public enum PlayerMarkers {
-    X ("\u001B[31m " + "X" + " \u001B[0m"),
-    O ("\u001B[34m " + "O" + " \u001B[0m");
+    X ("\u001B[31m X \u001B[0m"),
+    O ("\u001B[34m O \u001B[0m");
 
     private String display;
 
