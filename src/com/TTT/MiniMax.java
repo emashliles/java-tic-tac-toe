556c45b3b08b920eb4994f504a8b7d8efce774f0
EA <emashliles@gmail.com>
2017-02-10 13:35:24 +0000

    Test for winning move score.

--- src/com/TTT/MiniMax.java
+++ src/com/TTT/MiniMax.java
@@ -4,4 +4,8 @@ public class MiniMax {
     public int nextMove(Board board) {
         return -1;
     }
+
+    public int getScoreForMove(int move, Board board) {
+       return 0;
+    }
 }
---
+++ test/MiniMaxScoringTests.java
@@ -0,0 +1,44 @@
+import com.TTT.Board;
+import com.TTT.MiniMax;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MiniMaxScoringTests {
+
+    private MiniMax miniMax;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        miniMax = new MiniMax();
+        board = new Board(3);
+    }
+
+    @Test
+    public void calculates0ScoreForTie() {
+        board.placeMarker(0, "X");
+        board.placeMarker(1, "O");
+        board.placeMarker(2, "X");
+        board.placeMarker(3, "X");
+        board.placeMarker(4, "O");
+        board.placeMarker(5, "X");
+        board.placeMarker(6, "O");
+        board.placeMarker(8, "O");
+
+        assertEquals(0, miniMax.getScoreForMove(7,board));
+    }
+
+    @Test
+    public void calculates10ForWinningMove() {
+        board.placeMarker(0, "X");
+        board.placeMarker(1, "O");
+        board.placeMarker(2, "X");
+        board.placeMarker(3, "X");
+        board.placeMarker(4, "O");
+        board.placeMarker(5, "X");
+
+        assertEquals(10, miniMax.getScoreForMove(6, board));
+    }
+}
