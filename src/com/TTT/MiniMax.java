c3951840c706ea9ff63e7691fcd1cacc9a030556
EA <emashliles@gmail.com>
2017-02-15 14:13:08 +0000

    Delete everything.

--- src/com/TTT/MiniMax.java
+++ src/com/TTT/MiniMax.java
@@ -5,61 +5,14 @@ import java.util.List;
 
 public class MiniMax {
 
-    public int nextMove(Board board, String currentPlayerSymbol) {
-        List<Integer> scores = new ArrayList<>();
-        List<Integer> moves = new ArrayList<>();
-
-        for (int move : board.availableMoves()) {
-            Board clonedBoard = board.clone();
-
-            clonedBoard.placeMarker(move, currentPlayerSymbol);
-
-            scores.add(getScoreForMove(move, clonedBoard, currentPlayerSymbol, currentPlayerSymbol, changePlayerSymbol(currentPlayerSymbol), 1));
-            moves.add(move);
-        }
-
-        if (moves.size() == 1) {
-            return moves.get(0);
-        }
-
-        int winningMove = -200;
-        int winningScore = -200;
-        for (int i = 0; i < moves.size(); i++) {
-            if (scores.get(i) > winningScore) {
-                winningMove = moves.get(i);
-                winningScore = scores.get(i);
-            }
-        }
-
-        return winningMove;
+    public int nextMove(Board board, String currentPlayerSymbol){
+        return -1;
     }
 
     public int getScoreForMove(int move, Board board, String playerSymbol, String maxingPlayerSymbol, String minimizingPlayerSymbol, int depth) {
-        int bestScore = -200;
-
-        board.placeMarker(move, playerSymbol);
-        BoardEvaluator evaluator = new BoardEvaluator(board);
-
-        if (evaluator.evaluate() == GameState.Win && playerSymbol.equals(maxingPlayerSymbol)) {
-            return 10 - depth;
-        }
-
-        if (evaluator.evaluate() == GameState.Tie) {
-            return 0;
-        }
-
-        if (evaluator.evaluate() == GameState.Win && playerSymbol.equals(minimizingPlayerSymbol)) {
-            return depth - 10;
-        }
-
-        for (int nextMove : board.availableMoves()) {
-            Board clonedBoard = board.clone();
-            int score = getScoreForMove(nextMove, clonedBoard, changePlayerSymbol(playerSymbol), maxingPlayerSymbol, minimizingPlayerSymbol, depth++);
-            bestScore = score;
-        }
 
 
-        return bestScore;
+        return -1;
     }
 
     private String changePlayerSymbol(String currentPlayerSymbol) {
